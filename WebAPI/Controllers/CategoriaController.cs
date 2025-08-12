a84e7fb8ed98ed5a5eb1c1f8d999d19d17e2b68d
Jose <josehenriqui@hotmail.com>
2025-08-12 19:56:17 -0300

    Adicionado as controllers de despesa e categoria

---
+++ WebAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.InterfaceServicos;
+using Entities.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+
+        private readonly ICategoria _ICategoria;
+        private readonly ICategoriaServico _ICategoriaServico;
+
+        public CategoriaController(ICategoria ICategoria, ICategoriaServico ICategoriaServico)
+        {
+            _ICategoria = ICategoria;
+            _ICategoriaServico = ICategoriaServico;
+        }
+
+
+        [HttpGet("/api/ListarCategoriasUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListarCategoriasUsuario(string emailUsuario)
+        {
+            return _ICategoria.ListarCategoriaUsuario(emailUsuario);
+        }
+
+        [HttpPost("/api/AdicionarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> AdicionarCategoria(Categoria categoria)
+        {
+            await _ICategoriaServico.AdicionarCategoria(categoria);
+
+            return categoria;
+        }
+
+
+        [HttpPut("/api/AtualizarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> AtualizarCategoria(Categoria categoria)
+        {
+            await _ICategoriaServico.AtualizarCategoria(categoria);
+
+            return categoria;
+        }
+
+        [HttpGet("/api/ObterCategoria")]
+        [Produces("application/json")]
+        public async Task<object> ObterCategoria(int id)
+        {
+            return await _ICategoria.GetEntityById(id);
+        }
+
+        [HttpDelete("/api/DeletarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> DeletarCategoria(int id)
+        {
+            try
+            {
+                var categoria = await _ICategoria.GetEntityById(id);
+                await _ICategoria.Delete(categoria);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WebAPI/Controllers/DespesasController.cs
@@ -0,0 +1,88 @@
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServicos;
+using Domain.Servicos;
+using Entities.Entidades;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DespesasController : ControllerBase
+    {
+        private readonly IDespesa _IDespesa;
+        private readonly IDespesaServico _IDespesaServico;
+        public DespesasController(IDespesa IDespesa, IDespesaServico IDespesaServico)
+        {
+            _IDespesa = IDespesa;
+            _IDespesaServico = IDespesaServico;
+        }
+
+        [HttpGet("/api/ListarDespesasUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListarDespesasUsuario(string emailUsuario)
+        {
+            return await _IDespesa.ListarDespesasUsuario(emailUsuario);
+        }
+
+        [HttpPost("/api/AdicionarDespesa")]
+        [Produces("application/json")]
+        public async Task<object> AdicionarDespesa(Despesa despesa)
+        {
+            await _IDespesaServico.AdicionarDespesa(despesa);
+
+            return despesa;
+
+        }
+
+        [HttpPut("/api/AtualizarDespesa")]
+        [Produces("application/json")]
+        public async Task<object> AtualizarDespesa(Despesa despesa)
+        {
+            await _IDespesaServico.AtualizarDespesa(despesa);
+
+            return despesa;
+
+        }
+
+
+        [HttpGet("/api/ObterDespesa")]
+        [Produces("application/json")]
+        public async Task<object> ObterDespesa(int id)
+        {
+            return await _IDespesa.GetEntityById(id);
+        }
+
+
+        [HttpDelete("/api/DeletarDespesa")]
+        [Produces("application/json")]
+        public async Task<object> DeletarDespesa(int id)
+        {
+            try
+            {
+                var categoria = await _IDespesa.GetEntityById(id);
+                await _IDespesa.Delete(categoria);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [HttpGet("/api/CarregarGraficos")]
+        [Produces("application/json")]
+        public async Task<object> CarregarGraficos(string emailUsuario)
+        {
+            return await _IDespesaServico.CarregaGraficos(emailUsuario);
+        }
+
+
+    }
+}
